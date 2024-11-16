4bf7252b94c4588b5679bfa5eaf2dbd579498312
FuscaoPreto <102440579+FuscaoPreto@users.noreply.github.com>
2024-11-16 18:44:18 -0300

    Correct Setup

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1' 
-DESCRIPTION = 'Meu primeiro pacote em Python'
-LONG_DESCRIPTION = 'Meu primeiro pacote em Python com uma descrição um pouco mais longa'
+VERSION = '0.0.a' 
+DESCRIPTION = 'Um simples Pacote de Processamento de Imagens'
+LONG_DESCRIPTION = 'Um pacote que contém funções básicas para processamento de imagens'
 
 # Setting up
 setup(
@@ -13,13 +13,15 @@ setup(
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
-        install_requires=['numpy>=2.0.0','Python>=3.6'],
-        
+        install_requires=['numpy>=2.0.0'],  # Only package dependencies here
+        python_requires='>=3.6',  # Python version compatibility specified here
         keywords=['python', 'image processing', 'simple'],
         classifiers= [
             "Development Status :: 3 - Alpha",
             "Intended Audience :: Education",
             "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.11",
             "Operating System :: MacOS :: MacOS X",
             "Operating System :: Microsoft :: Windows",
         ]
